53dc4ebb3b81405d43eeeadb3bfc215996231599
Princeton Rose <decoyzpr@gmail.com>
2019-03-03 04:55:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def long_planeteer_calls(calls)# code an argument here
   calls.any? { |word| word.size > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
