d3ed636c569943dd69e244b3a2a172095eace0b4
Princeton Rose <decoyzpr@gmail.com>
2019-03-03 04:55:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(names)# code an argument here
   # Your code here
-  names.each_with_index { |name, index| puts "#{index}.  #{name}" }
+  names.each_with_index { |name, index| puts "#{index + 1}.  #{name}" }
 end
 
 def summon_captain_planet(elements)# code an argument here
@@ -16,5 +16,5 @@ end
 def find_the_cheese(food)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+
 end
