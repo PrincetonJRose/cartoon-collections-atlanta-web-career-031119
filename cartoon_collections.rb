b8808f8b39f411c0c68eebf181407b6f95e5ee3d
Princeton Rose <decoyzpr@gmail.com>
2019-03-03 05:11:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,12 @@ end
 def find_the_cheese(food)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-
+  for yum in food
+    for cheese in cheese_types
+      if yum == cheese
+        return cheese
+      end
+    end
+  end
+  return nil
 end
